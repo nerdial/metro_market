b2e3ad6381f472c0266749faf45c32dfb8a61c42
Vahid <v.mahdiun@yahoo.com>
2022-03-16 13:30:47 +0330

    Created offer test

---
+++ tests/OfferTest.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class OfferTest extends TestCase
+{
+    public function testCallOfferApi(): void
+    {
+        $this->assertTrue(true);
+    }
+}
